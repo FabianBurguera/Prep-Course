38f99c2aea78993e3bc6584b1b8d16083b1c9cfc
FabianBurguera <fabi.burguera@hotmail.com>
2022-11-27 22:55:18 -0300

    Homework actualizada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -180,18 +180,20 @@ function empiezaConNueve(n) {
 
 
 
-function todosIguales(arreglo) {
+function todosIguales(arreglo){
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
- for (var i=0 ; i<arreglo.lenght-1 ; i++){ 
- if (arreglo[i] !== arreglo[i+1]){
-  return false ;
- }}
-  return true ;
-
- 
+  for (var i=0 ; i< arreglo.length ; i++){
+  if (arreglo[i] !== arreglo[i+1]){
+    return false;
+  }
+  else {
+    return true ;
+  }
 }
+}
+
 
 
 function mesesDelAño(array) {
