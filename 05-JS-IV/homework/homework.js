dd6b271b0e3aa864697641cdc2cbb5dffc85959a
FabianBurguera <fabi.burguera@hotmail.com>
2022-12-17 12:29:22 -0300

    ejercicio 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -59,7 +59,12 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+var objt = { 
+  nombre : nombre,
+  email : email, 
+  password : password ,
+};
+return objt;
 }
 
 function tieneEmail (usuario) {
